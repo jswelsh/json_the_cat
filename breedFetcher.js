f728ebd6659e3f2f38f5cace91e309e5d12fac08
jswelsh <james.sinclair.welsh@gmail.com>
2019-07-05 03:46:50 +0000

    finished cat file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,15 @@
 const request = require('request');
-const parametersIn = process.argv.splice(2);
-  
-request(`https://api.thecatapi.com/v1/breeds/search?q=${parametersIn}`, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-      
-  const data = JSON.parse(body);
-  console.log(typeof(data));
-  if ((!data) || ((Object.entries(data).length === 0))) {
-    console.log("ERROR: breed not found!");
-    return;
-  } else console.log(data[0].description);
 
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, resp, body) => {
+    if (!error) callback(error);
+    const data = JSON.parse(body);
 
+    if ((!data) || ((Object.entries(data).length === 0))) {
+      callback(error);
+      return;
+    } else callback(error, data[0].description);
+  });
+};
+
+module.exports = { fetchBreedDescription };
